2d53e2e4d7bd3cc4c4014536b2e47b50fddad225
Taisson S de Oliveira <taisson.oliveira@gmail.com>
2018-12-13 15:33:16 -0200

    Arquivo que o Richard modificou

--- _php/processaUpload.php
+++ _php/processaUpload.php
@@ -6,23 +6,23 @@ session_start();
 
 
 //inserindo pdf
-$pasta = "../alunos/";
-$pasta ="{$pasta}{$dados['matricula']}";
-$allow = array('pdf');
-$temp = explode(".",$_FILES['arquivo']['name']);
-$extension=end($temp);
-$upload_file = $_FILES['arquivo']['name'];
-if(move_uploaded_file($_FILES['arquivo']['tmp_name'],"$pasta".$_FILES['arquivo']['name'])){
-	$query = mysqli_query($conectando,"insert into arquivo(nome_documento) values('".$upload_file."')");
-	echo "<script>
-		alert('PDF enviado com sucesso');
-		window.location.href ='usuario.php';
-		</script>
-		";
-}else{
-	"<script>
-		alert('Erro ao enviar');
-		window.location.href ='usuario.php';
-		</script>";
-}
+
+	$allow = array('pdf');
+	$temp = explode(".",$_FILES['arquivo']['name']);
+	$extension=end($temp);
+	$upload_file = $_FILES['arquivo']['name'];
+	if(move_uploaded_file($_FILES['arquivo']['tmp_name'],"../alunos/matricula/".$_FILES['arquivo']['name'])){
+		$query = mysqli_query($conectando,"insert into arquivo(nome_documento) values('".$upload_file."')");
+		echo "<script>
+			alert('PDF enviado com sucesso');
+			window.location.href ='usuario.php';
+			</script>
+			";
+	}else{
+		"<script>
+			alert('Erro ao enviar');
+			window.location.href ='usuario.php';
+			</script>
+			";
+	}
 ?>
